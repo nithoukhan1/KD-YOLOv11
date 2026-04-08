aca74ac61fdb1db506b2296df28d234e95b26f5e
UltralyticsAssistant <web@ultralytics.com>
2026-04-08 12:04:05 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -128,7 +128,7 @@ def bbox_iou(
 
     # IoU
     iou = inter / union
-    
+
     if CIoU or DIoU or GIoU:
         cw = b1_x2.maximum(b2_x2) - b1_x1.minimum(b2_x1)  # convex (smallest enclosing box) width
         ch = b1_y2.maximum(b2_y2) - b1_y1.minimum(b2_y1)  # convex height
